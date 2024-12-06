5a0f456c44c8c91b8088222d8047ae5bbdaa0443
Ex1s9 <sadovskyroma@gmail.com>
2024-12-06 17:58:00 +0300

    refactor skill

--- src/UI/Main/Skills/Hard skill/hard.tsx
+++ src/UI/Main/Skills/Hard skill/hard.tsx
@@ -1,39 +1,48 @@
 import "./hard.css";
 export const Hard = () => {
    return (
-      <section className="hard-skill">
-         <div className="skills-text">
-            <h2>
-               <span className="other-color">Hard</span> skills
-            </h2>
-            <h2>
-               <span className="other-color">Soft</span> skills
-            </h2>
-         </div>
+      <section className="skill">
+         <div className="skill-inner">
+            <div className="skill-column">
+               <h2 className="skill-text">
+                  <span className="other-color">Hard</span> skills
+               </h2>
+               <div className="skill-cont1">
+                  <ul className="skill-list">
+                     <li className="skill-item"><a className="skill-item-text">HTML (HTML5)</a></li>
+                     <li className="skill-item"><a className="skill-item-text">CSS, SCSS/SASS</a></li>
+                     <li className="skill-item"><a className="skill-item-text">JavaScript, TypeScript</a></li>
+                     <li className="skill-item"><a className="skill-item-text">React</a></li>
+                     <li className="skill-item"><a className="skill-item-text">Redux (Redux Toolkit)</a></li>
+                     <li className="skill-item"><a className="skill-item-text">React Router, React-Hook-Form</a></li>
+                     <li className="skill-item"><a className="skill-item-text">Vite, Webpack</a></li>
+                     <li className="skill-item"><a className="skill-item-text">Redux (Redux Toolkit)</a></li>
+                     <li className="skill-item"><a className="skill-item-text">GIT</a></li>
+                     <li className="skill-item"><a className="skill-item-text">Figma</a></li>
+                  </ul>
+               </div>
+            </div>
 
-         <div className="skills">
-            <div className="skills-cont1">
-               <ul className="skills-list">
-                  <li className="skills-item">React</li>
-                  <li className="skills-item">TypeScript</li>
-                  <li className="skills-item">CSS/SASS</li>
-                  <li className="skills-item">Node.js</li>
-                  <li className="skills-item">Express.js</li>
-               </ul>
-            </div>
-            
-            <div className="skills-cont2">
-               <ul className="skills-list">
-                  <li className="skills-item">React</li>
-                  <li className="skills-item">TypeScript</li>
-                  <li className="skills-item">CSS/SASS</li>
-                  <li className="skills-item">Node.js</li>
-                  <li className="skills-item">Express.js</li>
-               </ul>
+            <div className="skill-column">
+               <h2 className="skill-text">
+                  <span className="other-color">Soft</span> skills
+               </h2>
+               <div className="skill-cont1">
+                  <ul className="skill-list">
+                     <li className="skill-item"><a className="skill-item-text">HTML (HTML5)</a></li>
+                     <li className="skill-item"><a className="skill-item-text">CSS, SCSS/SASS</a></li>
+                     <li className="skill-item"><a className="skill-item-text">JavaScript, TypeScript</a></li>
+                     <li className="skill-item"><a className="skill-item-text">React</a></li>
+                     <li className="skill-item"><a className="skill-item-text">Redux (Redux Toolkit)</a></li>
+                     <li className="skill-item"><a className="skill-item-text">React Router, React-Hook-Form</a></li>
+                     <li className="skill-item"><a className="skill-item-text">Vite, Webpack</a></li>
+                     <li className="skill-item"><a className="skill-item-text">Redux (Redux Toolkit)</a></li>
+                     <li className="skill-item"><a className="skill-item-text">GIT</a></li>
+                     <li className="skill-item"><a className="skill-item-text">Figma</a></li>
+                  </ul>
+               </div>
             </div>
          </div>
-         
-         
       </section>
    )
 }
